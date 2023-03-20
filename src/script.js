cf2fb6cf0de7a781f000ca715020c8c3ce058168
Ruthe077 <122920984+Ruthe077@users.noreply.github.com>
2023-03-20 15:17:10 -0500

    set date and time js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
-alert("Hello!");
+//set current date and time
+
+let now = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = (months[now.getMonth()]);
+
+let date = document.querySelector("#date");
+date.innerHTML =  (`${month} ${now.getDate()}`);
+
+let time = document.querySelector("#time");
+time.innerHTML = (`${now.getHours()}:${now.getMinutes()}`);
+
+day = document.querySelector("#day");
+day.innerHTML = days[now.getDay()];
+
+
+//integrating weather API
