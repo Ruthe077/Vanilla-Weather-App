c3d205d6e7f080320fe377b8ae5df0c9e1eca658
Ruthe077 <122920984+Ruthe077@users.noreply.github.com>
2023-03-20 16:14:22 -0500

    Added weather API and some styling

--- src/script.js
+++ src/script.js
@@ -20,13 +20,48 @@ let months = [
 let month = (months[now.getMonth()]);
 
 let date = document.querySelector("#date");
-date.innerHTML =  (`${month} ${now.getDate()}`);
+date.innerHTML =  (`${month.toUpperCase()} ${now.getDate()}`);
 
 let time = document.querySelector("#time");
 time.innerHTML = (`${now.getHours()}:${now.getMinutes()}`);
 
 day = document.querySelector("#day");
-day.innerHTML = days[now.getDay()];
+day.innerHTML = days[now.getDay()].toUpperCase();
 
 
 //integrating weather API
+
+function getTemp(response) {
+  console.log(response);
+  let temp = Math.round(response.data.temperature.current);
+  let humidity = document.querySelector("#humidity")
+  let location = document.querySelector("#city");
+  let city = response.data.city;
+  let cityTemp = document.querySelector("#current-day");
+  let condition = document.querySelector("#description");
+  let wind = document.querySelector("#wind");
+  let icon = document.querySelector("#icon");
+  icon.setAttribute("src", response.data.condition.icon_url);
+  condition.innerHTML = response.data.condition.description.toUpperCase();
+  humidity.innerHTML = response.data.temperature.humidity;
+  cityTemp.innerHTML = `${temp}°`;
+  location.innerHTML = city;
+ wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+function changeCity(event) {
+  event.preventDefault();
+  let apiKey = "3at2b7b503ccbeb5e43637ffc4ae05oa";
+  let unit = "units=metric";
+  let city = document.querySelector("#city-input");
+  city = (city.value);
+  let url = "https://api.shecodes.io/weather/v1/current?query=";
+  axios.get(`${url}${city}&key=${apiKey}&${unit}`).then(getTemp);
+}
+
+
+  
+
+let form = document.querySelector("form");
+form.addEventListener("submit", changeCity);
